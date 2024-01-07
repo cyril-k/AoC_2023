f069ee3eed226a1ca0601f211a3f3db4e4ecfde1
kkondratenko <kkondratenko@assystem.com>
2024-01-07 19:35:29 +0100

    day-21 not working

--- day-21/src/bin/part1.rs
+++ day-21/src/bin/part1.rs
@@ -1,10 +1,6 @@
-use std::collections::{BinaryHeap, HashMap, HashSet};
-use std::cmp::Ordering;
-use std::cmp::Reverse;
-
+use std::collections::{HashSet, VecDeque};
 use ndarray::{self, Array2};
 
-const MAX_COST: usize = 10000;
 
 fn main () {
     let input = include_str!("./part1.txt");
@@ -24,109 +20,66 @@ fn part1(input: &str) -> String {
             .unwrap()
     });
 
-    println!("{:?}", maze_array);
+    // println!("{:?}", maze_array);
+    let start = maze_array.indexed_iter().find_map(|(index, c)| {
+        if *c == 'S' {
+            Some(Node::new(index))
+        } else { None }
+    }).unwrap();
 
-    let res = a_star_search(Node::new((5, 5)), Node::new((0, 0)), &maze_array);
+    let res = bfs_search(start, 64, &maze_array);
     println!("{:?}", res);
 
     "output".to_string()
 }
 
-
-#[derive(Eq, PartialEq, Debug, Clone)]
+#[derive(Eq, PartialEq, Debug, Clone, Hash)]
 struct Node {
-    position: (usize, usize),
-    g_cost: usize,
-    h_cost: usize,
+    position: (usize, usize)
 }
 
 impl Node {
     fn new(position: (usize, usize)) -> Self {
-        Node { position, g_cost: MAX_COST, h_cost: 0 }
-    }
-
-    fn f_cost(&self) -> usize {
-        self.g_cost + self.h_cost
+        Node { position }
     }
 }
 
-impl Ord for Node {
-    fn cmp(&self, other: &Self) -> Ordering {
-        other.f_cost().cmp(&self.f_cost()).reverse()
-    }
-}
-
-impl PartialOrd for Node {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-fn manhattan_distance(start: (usize, usize), goal: (usize, usize)) -> usize {
-    ((start.0 as isize - goal.0 as isize).abs() + (start.1 as isize - goal.1 as isize).abs()) as usize
-}
-
-fn get_neighbors(node: &Node, maze: &Array2<char>) -> Vec<Node> {
+fn get_neighbors(position: &(usize, usize), maze: &Array2<char>) -> Vec<(usize, usize)> {
     let directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]; // r, d, l, u
     directions.iter().filter_map(|&(dx, dy)| {
-        let new_pos = ((node.position.0 as isize + dx) as usize, (node.position.1 as isize + dy) as usize);
+        let new_pos = ((position.0 as isize + dx) as usize, (position.1 as isize + dy) as usize);
         maze.get(new_pos).and_then(|&cell| {
-            if cell == '.' || cell == 'S' { Some(Node::new(new_pos)) } else { None }
+            if cell == '.' || cell == 'S' { Some(new_pos) } else { None }
         })
     }).collect()
 }
 
-fn reconstruct_path(came_from: HashMap<(usize, usize), (usize, usize)>, mut current_pos: (usize, usize)) -> Vec<(usize, usize)> {
-    let mut path = vec![current_pos];
-    println!("came_from: {:?}", came_from);
-    while let Some(parent_pos) = came_from.get(&current_pos) {
-        println!("parent_pos: {:?}", parent_pos);
-        path.push(*parent_pos);
-        current_pos = *parent_pos;
-        break
-    }
-    path.reverse();
-    path
-}
+fn bfs_search(start: Node, target_step: usize, maze: &Array2<char>) -> usize {
+    let mut queue = VecDeque::new();
+    queue.push_back((start, 0));
 
-fn a_star_search(start: Node, goal: Node, maze: &Array2<char>) -> Option<Vec<(usize, usize)>> {
-    let mut open_set = BinaryHeap::new();
-    let mut open_set_tracker = HashSet::new();
-    let mut came_from = HashMap::new();
+    let mut current_tracker = HashSet::new();
 
-    open_set_tracker.insert(start.position);
-    open_set.push(Reverse(start));
+    while let Some((current, step)) = queue.pop_front() {
 
-    let mut counter: usize = 0;
-
-    while let Some(Reverse(current)) = open_set.pop() {
-        println!("current node: {:?}", current);
-        // println!("goal node: {:?}", goal);
-        open_set_tracker.remove(&current.position);
-        if current.position == goal.position {
-            return Some(reconstruct_path(came_from, current.position));
-        }
-
-        counter += 1;
-        if counter > 6 {
+        if step == target_step {
+            current_tracker.insert(current);
+            continue;
+        } else if step > target_step {
             break;
         }
 
-        for mut neighbor in get_neighbors(&current, maze) {
-            println!("current neighbor: {:?}", neighbor);
-            let tentative_g_score = current.g_cost - 1;
-            println!("tentative_g_score < neighbor.g_cost: {:?}", tentative_g_score < neighbor.g_cost);
-            if tentative_g_score < neighbor.g_cost {
-                came_from.insert(neighbor.position, current.position);
-                neighbor.g_cost = tentative_g_score;
-                neighbor.h_cost = manhattan_distance(neighbor.position, goal.position);
-                if !open_set_tracker.contains(&neighbor.position) {
-                    open_set_tracker.insert(neighbor.position);
-                    open_set.push(Reverse(neighbor));
-                }
-            }
+        for neighbor_pos in get_neighbors(&current.position, maze) {
+                queue.push_back((Node::new(neighbor_pos), step + 1));
         }
     }
-    println!("open_set_tracker: {:?}", open_set_tracker);
-    None
+
+    let mut viz_maze = maze.clone();
+
+    for node in current_tracker.iter() {
+        *viz_maze.get_mut(node.position).unwrap() = 'O';
+    }
+    println!("viz_maze:\n {}", viz_maze);
+
+    current_tracker.len()
 }
