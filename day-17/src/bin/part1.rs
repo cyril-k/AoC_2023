709c978e2a237e449f9291c17341c9d8391b91f7
Cyril Kondratenko <kkn1993@gmail.com>
2024-01-13 00:37:32 +0100

    day-17 wip

--- day-17/src/bin/part1.rs
+++ day-17/src/bin/part1.rs
@@ -1,8 +1,9 @@
-use std::{collections::{HashMap, BinaryHeap}, os::linux::raw};
+use std::collections::{HashMap, BinaryHeap};
 use ndarray::{self, Array2};
 use std::cmp::Ordering;
 use std::hash::{Hash, Hasher};
-use std::collections::hash_map::DefaultHasher;
+
+use std::cmp::Reverse;
 
 fn main () {
     let input = include_str!("./part1.txt");
@@ -24,212 +25,140 @@ fn part1(input: &str) -> String {
                 .map(|c| c.to_digit(10).unwrap() as usize))
             .collect())
         .expect("Failed to reshape into 2D array");
+        
+    let start = (0, 0);
+    let goal = (array.nrows()-1, array.ncols()-1);
+    let cost = a_star(&array, start, goal, 3);
 
-    let graph = graph_from_array(array);
-    
-    let start = Node::new((0, 0));
-    let distances = dijkstra(&graph, start, 3);
-
-    println!("{:?}", distances);
+    println!("{:?}", cost);
 
     "output".to_string()
     // sum.to_string()
 
 }
 
-#[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
-enum Position {
+#[derive(Clone, Copy, Eq, PartialEq)]
+enum Direction {
     Up,
     Down,
     Left,
     Right,
-    None,
+    None, 
 }
 
-type Index = (usize, usize);
-
-#[derive(Clone, Copy, Debug)]
-struct Node {
-    index: Index,
-    pos: Position,
+#[derive(Clone, Copy, Eq, PartialEq)]
+struct State {
+    position: (usize, usize),
+    direction: Direction,
     consecutive: usize,
 }
 
-impl Hash for Node {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.index.hash(state);
-    }
-}
-
-impl PartialEq for Node {
-    fn eq(&self, other: &Self) -> bool {
-        self.index == other.index
-    }
-}
-
-impl Eq for Node {}
-
-impl Node {
-    fn new(index: Index) -> Self {
-        Node { index, pos: Position::None, consecutive: 0 }
-    }
-
-    fn with_direction(index: Index, pos: Position, consecutive: usize) -> Self {
-        Node { index, pos, consecutive }
-    }
-
-    fn pos(&self, other: &Node) -> Position {
-        if self.index.0 > other.index.0 {
-            Position::Up
-        } else if self.index.0 < other.index.0 {
-            Position::Down
-        } else if self.index.1 > other.index.1 {
-            Position::Left
-        } else if self.index.1 < other.index.1 {
-            Position::Right
-        } else {
-            Position::None
-        }
-    }
-}
-
-#[derive(Clone, Eq, PartialEq, Hash, Debug)]
-struct Edge {
-    node: Node,
-    cost: usize,
-}
-
-impl Ord for Edge {
+impl Ord for State {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.cost.cmp(&other.cost)
+        self.position.cmp(&other.position)
     }
 }
 
-impl PartialOrd for Edge {
+impl PartialOrd for State {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-type Graph = HashMap<Node, Vec<Edge>>;
+fn neighbors(current: State, rows: usize, cols: usize, max_consecutive: usize) -> Vec<State> {
+    let mut result = Vec::new();
+    let (row, col) = current.position;
 
+    let directions = [
+        ((row.wrapping_sub(1), col), Direction::Up),
+        ((row + 1, col), Direction::Down),
+        ((row, col.wrapping_sub(1)), Direction::Left),
+        ((row, col + 1), Direction::Right),
+    ];
 
-fn graph_from_array(array: Array2<usize>) -> Graph {
-    let mut graph = Graph::new();
-    let (rows, cols) = array.dim();
-
-    array
-        .indexed_iter()
-        .for_each(|((row, col), _ )| {
-            let mut edges = Vec::new();
-
-            if col > 0 { // Left
-                update_edges(row, col - 1, &mut edges, &array);
+    for &(new_pos, new_dir) in &directions {
+        if new_pos.0 < rows && new_pos.1 < cols {
+            if new_dir != current.direction && current.consecutive < max_consecutive {
+                result.push(State {
+                    position: new_pos,
+                    direction: new_dir,
+                    consecutive: 1,
+                });
+            } else if new_dir == current.direction {
+                result.push(State {
+                    position: new_pos,
+                    direction: new_dir,
+                    consecutive: current.consecutive + 1,
+                });
             }
-            if col < rows - 1 { // Right
-                update_edges(row, col + 1, &mut edges, &array);
-            }
-            if row > 0 { // Down
-                update_edges(row - 1, col, &mut edges, &array);
-            }
-            if row < cols - 1 { // Up
-                update_edges(row + 1, col, &mut edges, &array);
-            }
-
-            graph.insert(Node::new((row, col)), edges);
-        });
-    graph
-}
-
-fn update_edges(
-    row: usize, 
-    col: usize, 
-    edges: &mut Vec<Edge>, 
-    array: &Array2<usize>) {
-    
-    let index: Index = (row, col);
-    edges.push(Edge { node: Node::new(index), cost: *array.get(index).unwrap() });
-}
-
-fn dijkstra(graph: &Graph, start: Node, max_consecutive: usize) -> HashMap<Node, usize> {
-    let mut distances: HashMap<Node, usize> = HashMap::new();
-    let mut heap = BinaryHeap::new();
-
-    *distances.entry(start).or_insert(usize::MAX) = 0;
-    heap.push(Edge { node: start, cost: 0 });
-
-    while let Some(Edge { node, cost }) = heap.pop() {
-        if cost > *distances.entry(node).or_insert(usize::MAX) {
-            continue;
-        }
-
-        for edge in graph.get(&node).unwrap() {
-            let new_consecutive = if edge.node.pos == node.pos {
-                node.consecutive + 1
-            } else {
-                1
-            };
-
-            println!("node {:?}, consecutive count {}", edge.node, new_consecutive);
-
-            if new_consecutive <= max_consecutive {
-                // continue; // Skip this edge if it exceeds the consecutive move limit
-                let next_node = Node::with_direction(edge.node.index, edge.node.pos, new_consecutive);
-                let next = Edge { node: next_node, cost: cost + edge.cost };
-    
-                if next.cost < *distances.entry(next.node).or_insert(usize::MAX) {
-                    *distances.entry(next.node).or_insert(usize::MAX) = next.cost;
-                    heap.push(next);
-                }
-            }
-
         }
     }
 
-    distances
+    result
 }
 
-// fn dijkstra(graph: &Graph, start: Node) -> HashMap<Node, usize> {
-//     let mut distances: HashMap<Node, usize> = HashMap::new();
-//     let mut heap = BinaryHeap::new();
+fn manhattan_d(node: &(usize, usize), goal: &(usize, usize)) -> usize {
+    let dx = (node.0 as isize - goal.0 as isize).abs();
+    let dy = (node.1 as isize - goal.1 as isize).abs();
+    (dx + dy) as usize
+}
 
-//     *distances.entry(start).or_insert(usize::MAX) = 0;
-//     heap.push(Edge { node: start, cost: 0 });
+fn a_star(array: &Array2<usize>, start: (usize, usize), goal: (usize, usize), max_consecutive: usize) -> usize {
+    let mut distances: HashMap<(usize, usize), usize> = HashMap::new();
+    let mut heap = BinaryHeap::new();
+    let mut came_from: HashMap<(usize, usize), (usize, usize)> = HashMap::new();
 
-//     // let mut dir_counter: HashMap<Position, usize> = HashMap::new();
-//     // let mut dir_map: HashMap<Node, Vec<Position>> = HashMap::new();
-//     // Vec<Position> = Vec::new();
+    distances.insert(start, 0);
 
-//     while let Some(Edge {node, cost}) = heap.pop() {
-//         println!("current node {:?}", node);
-//         if cost > *distances.entry(node).or_insert(usize::MAX) {
-//             continue;
-//         };
+    let start_state = State {
+        position: start,
+        direction: Direction::None,
+        consecutive: 0,
+    };
 
-
-//         for edge in graph.get(&node).unwrap() {
-//             // let dir = node.pos(&edge.node);
-//             // let dir_counter = dir_map.entry(edge.node).or_insert(vec![]);
-//             // // *dir_counter.entry(dir).or_insert(0) += 1;
-//             // if dir_counter.contains(&dir) {
-//             //     dir_counter.push(dir)
-//             // } else {
-//             //     *dir_counter = vec![dir, ] // reset direction counter
-//             // }
-//             // println!("{:?}", dir_counter); 
-            
-//             // if dir_counter.len() <= 3 {
-//                 let next = Edge { node: edge.node, cost: cost + edge.cost};
+    heap.push(Reverse((0 + manhattan_d(&start, &goal), start_state)));
     
-//                 if next.cost < *distances.entry(next.node).or_insert(usize::MAX) {
-//                     *distances.entry(next.node).or_insert(usize::MAX) = next.cost;
-//                     heap.push(next);
-//                 }
+    while let Some(Reverse((_, current))) = heap.pop() {
+        if current.position == goal {
+            break;
+        }
 
-//             // }
+        for neighbor in neighbors(current, array.nrows(), array.ncols(), max_consecutive) {
+            let next_cost = distances[&current.position] + array.get(neighbor.position).unwrap();
 
-//         }
-//     }
+            if next_cost < *distances.entry(neighbor.position).or_insert(usize::MAX) {
+                distances.insert(neighbor.position, next_cost);
+                came_from.insert(neighbor.position, current.position);
+                let total_cost = next_cost + manhattan_d(&neighbor.position, &goal);
+                heap.push(Reverse((total_cost, neighbor)));
+            }
+        }
 
-//     distances
-// }
+        
+    }
+    println!("distances {:?}", distances);
+    // distances
+    calculate_total_cost(array, &distances, &came_from, start, goal)
+}
+
+fn calculate_total_cost(
+    array: &Array2<usize>,
+    distances: &HashMap<(usize, usize), usize>,
+    came_from: &HashMap<(usize, usize), (usize, usize)>,
+    start: (usize, usize),
+    goal: (usize, usize),
+) -> usize {
+    let mut total_cost = 0;
+    let mut current = goal;
+    println!("goal {:?}", goal);
+    while let Some(&previous) = came_from.get(&current) {
+        total_cost += array.get(current).unwrap_or(&0);
+        // println!("total_cost {}", total_cost);
+        if previous == start {
+            break;
+        }
+        current = previous;
+    }
+
+    total_cost - array.get(start).unwrap()
+}
