fa64d213e8b4bb66b8cbf83db0665fd7fb952eb4
Cyril Kondratenko <kkn1993@gmail.com>
2024-01-13 13:08:01 +0100

    day-23 wip

--- day-23/src/bin/part1.rs
+++ day-23/src/bin/part1.rs
@@ -1,3 +1,5 @@
+use ndarray::{self, Array2};
+
 fn main () {
     let input = include_str!("./part1.txt");
     let output = part1(input);
@@ -8,4 +10,33 @@ fn part1(input: &str) -> String {
 
 
     "output".to_string()
+}
+
+fn get_neighbors(pos: &(usize, usize), array: &Array2<char>) -> Vec<(usize, usize)> {
+    let mut neighbours = Vec::new();
+    let directions: [(isize, isize); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)]; // u, d, l, r
+
+    for dir in directions {
+        let new_pos = (pos.0.wrapping_add_signed(dir.0), pos.1.wrapping_add_signed(dir.1));
+        if let Some(tile) = array.get(new_pos) {
+            match tile {
+                '.' => neighbours.push(new_pos.clone()),
+                'v' => if dir != (-1, 0) {
+                    neighbours.push(new_pos.clone());
+                },
+                '^' => if dir != (1, 0) {
+                    neighbours.push(new_pos.clone());
+                },
+                '>' => if dir != (0, -1) {
+                    neighbours.push(new_pos.clone());
+                },
+                '<' => if dir != (0, 1) {
+                    neighbours.push(new_pos.clone());
+                },
+                _ => (),
+            }
+        }
+    }
+
+    neighbours
 }
